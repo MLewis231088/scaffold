d0a4e30f7ddbc9a882759ff8a5a0c7f233bb2dcb
Malvica Lewis <malvicalewis.datascientist@gmail.com>
2021-10-18 12:31:41 +0000

    Adding latest build

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
-def add(x, y):
-    return x + y
+def toyou(x):
+    return f"hi {x}"
 
-# var = 
-result = add(1, 2)
+def add(x):
+    return x + 1
 
-print(f"The sum of 1 and 2 is: {result}")
+def subtract(x):
+    return x - 1
+    
